be9479cc8ae419518e2de788459fe758ea258bd6
lucas2003a <lucasatuncar1@gmail.com>
2023-11-09 17:13:48 -0500

    spu_cronograma

--- databases/SISCOMPU_ejecuciones.sql
+++ databases/SISCOMPU_ejecuciones.sql
@@ -4,4 +4,9 @@
 USE SISCOMPU;
 
 CALL spu_usuarios_login('adriana@gmail.com');
-CALL spu_usuarios_registrar('Adriana', 'Durand Buenamarca', 'Administrador', 'SENATI123', 'adriana@gmail.com', NULL)
+CALL spu_usuarios_registrar('Adriana', 'Durand Buenamarca', 'Administrador', 'SENATI123', 'adriana@gmail.com', NULL);
+CALL spu_mantenimiento_listar();
+CALL spu_mantenimiento_registrar();
+CALL spu_mantenimiento_modificar();
+CALL spu_mantenimiento_eliminar();
+
--- databases/SISCOMPU_procedimientos.sql
+++ databases/SISCOMPU_procedimientos.sql
@@ -40,9 +40,79 @@ BEGIN
 END $$*/
 
 -- -------------------------------------------------------------------------------------
--- ---------------- Procedimientos Alamacenados MANTENIMIENTO --------------------------
+-- ---------------- Procedimientos Alamacenados CRONOGRAMAS --------------------------
 -- -------------------------------------------------------------------------------------
 
+DROP PROCEDURE IF EXISTS spu_cronogramas_listar;
+DELIMITER $$
+CREATE PROCEDURE spu_cronogramas_listar()
+BEGIN
+	SELECT
+		cro.idcronograma,
+        equ.modelo_equipo,
+        equ.numero_serie,
+        cro.tipo_mantenimiento,
+        cro.estado,
+        cro.fecha_programada
+    FROM cronogramas as cro
+    INNER JOIN equipos as equ on equ.idequipo = cro.idequipo
+    WHERE
+		cro.inactive_at IS NULL;
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS spu_cronogramas_registrar;
+DELIMITER $$
+CREATE PROCEDURE spu_cronogramas_registrar
+(
+	in _idequipo			INT,
+    in _tipo_mantenimiento 	VARCHAR(45),
+    in _estado				VARCHAR(10),
+    in _fecha_programada 	DATETIME
+)
+BEGIN
+	INSERT INTO cronogramas
+		(idequipo,tipo_mantenimiento,estado,fecha_programada)
+		VALUES
+        (_idequipo,_tipo_mantenimiento,_estado,_fecha_programada);
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS spu_cronogramas_modificar;
+DELIMITER $$
+CREATE PROCEDURE spu_cronogramas_modificar
+(
+	in _idcronograma		INT,
+	in _idequipo			INT,
+    in _tipo_mantenimiento 	VARCHAR(45),
+    in _estado				VARCHAR(10),
+    in _fecha_programada 	DATETIME
+)
+BEGIN
+	UPDATE cronogramas SET
+		idequipo 			= _idequipo,			
+		tipo_mantenimiento 	= _tipo_mantenimiento,
+		estado 				= _estado,
+		fecha_programada 	=_fecha_programada
+	WHERE
+		idcronograma = _idcronograma;
+END $$
+DELIMITER ;
+
+
+/*
+DROP PROCEDURE IF EXISTS;
+DELIMITER $$
+CREATE PROCEDURE()
+BEGIN
+END $$
+DELIMITER ;
+*/
+
+-- -------------------------------------------------------------------------------------
+-- ---------------- Procedimientos Alamacenados MANTENIMIENTO --------------------------
+-- -------------------------------------------------------------------------------------
+-- FALTA INSERTAR DATOS
 DROP PROCEDURE IF EXISTS spu_mantenimiento_listar;
 DELIMITER $$
 CREATE PROCEDURE spu_mantenimiento_listar()
@@ -62,13 +132,46 @@ BEGIN
 END $$
 DELIMITER ;
 
-CALL spu_mantenimiento_listar();
-/*
-DROP PROCEDURE IF EXISTS;
+DROP PROCEDURE IF EXISTS spu_mantenimiento_registrar;
 DELIMITER $$
-CREATE PROCEDURE()
+CREATE PROCEDURE spu_mantenimiento_registrar
+(
+	in _idusuario 	INT,
+    in _idcronograma INT,
+    in _descripcion  VARCHAR(300)
+)
+BEGIN
+	INSERT INTO matenimiento 
+		(idusuario,idcronograma,descripcion)
+        VALUES
+        (_idusuario,_idcronograma,_descripcion);
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS spu_mantenimiento_modificar;
+DELIMITER $$
+CREATE PROCEDURE spu_mantenimiento_modificar
+(
+	in _idmantenmimiento	INT,
+    in _idusuario			INT,
+    in _idcronograma        INT,
+    in _descripcion			VARCHAR(300)
+)
+	UPDATE mantenimiento SET
+		idusuario	 = _idusuario,
+		idcronograma =_idcronograma,
+		descripcion  =_descripcion		
+	WHERE
+		idmantenimiento = _idmantenmimiento	;
 BEGIN
 END $$
 DELIMITER ;
-*/
+
+DROP PROCEDURE IF EXISTS spu_mantenimiento_eliminar;
+DELIMITER $$
+CREATE PROCEDURE spu_mantenimiento_eliminar(in _idmantenimiento INT)
+BEGIN
+	UPDATE mantenimiento SET
+		inactive_at = now();
+END $$
 
