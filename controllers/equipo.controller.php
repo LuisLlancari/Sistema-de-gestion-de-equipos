94d1fae94813f0c3412eaad79c37bc9dd093caaf
lucas2003a <lucasatuncar1@gmail.com>
2023-11-11 03:30:49 -0500

    crud equipo

---
+++ controllers/equipo.controller.php
@@ -0,0 +1,76 @@
+<?php
+
+date_default_timezone_set("America/Lima");
+
+require_once "../models/Equipo.php";
+
+if(isset($_POST['operacion'])){
+
+    $equipo = new Equipo();
+
+    switch ($_POST['operacion']){
+        case 'listar':
+        
+            echo json_encode($equipo->listar());
+            break;
+        
+        case 'registrar':
+
+            $hoy = date("dmYhis");
+
+            $nombreImagen = sha1($hoy) . ".jpg";
+
+            $datosEnviar = [
+
+                "idcategoria"   => $_POST['idcategoria'],
+                "idmarca"       => $_POST['idmarca'],
+                "idusuario"     => $_POST['idusuario'],
+                "modelo_equipo" => $_POST['modelo_equipo'],
+                "numero_serie"  => $_POST['numero_serie'],
+                "imagen"        =>  ""/* $nombreImagen */
+            ];
+
+            /* if(move_uploaded_file($_FILES['imagen']['tmp_name'], "../images/" . $nombreImagen)){
+
+                $datosEnviar['imagen'] = $nombreImagen;
+            }
+ */
+            echo json_encode($equipo->registrar($datosEnviar));
+
+            break;
+        
+        case 'modificar':
+
+            $hoy = date("hmsYis");
+
+            $nombreImagen = sha1($hoy) . ".jpg";
+
+            $datosEnviar = [
+
+                "idequipo"      => $_POST['idequipo'],
+                "idcategoria"   => $_POST['idcategoria'],
+                "idmarca"       => $_POST['idmarca'],
+                "idusuario"      => $_POST['idusuario'],
+                "modelo_equipo" => $_POST['modelo_equipo'],
+                "numero_serie"  => $_POST['numero_serie'],
+                "imagen"        => $nombreImagen
+            ];
+
+            /* 
+            if(move_uploaded_file($__FILES['imagen'], "../images/" . $nombreImagen)){
+                
+                $datosEnviar = ['imagen'];
+            } */
+
+            echo json_encode($equipo->modificar($datosEnviar));
+            break;
+        
+        case 'eliminar':
+
+            $datosEnviar = ["idequipo" => $_POST['idequipo']];
+
+            echo json_encode($equipo->eliminar($datosEnviar));
+
+            break; 
+    }
+}
--- models/Equipo.php
+++ models/Equipo.php
@@ -6,17 +6,86 @@ class Equipo extends Conexion{
 
     private $conexion;
 
-    public function __COSTRUCT(){
+    public function __CONSTRUCT(){
         $this->conexion = parent::getConexion();
     }
 
+
+    /**
+     * MÉTODO QUE NOS LISTA TODOS LOS DATOS DE LA CONSULTA DE LA BASE DE DATOS DE LA TABLA EQUIPOS
+     */
     public function listar(){
 
         try{
+            $consulta = $this->conexion->prepare("CALL spu_equipos_listar()");
+            $consulta->execute();
+
+            return $consulta->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch(Exception $e){
+            die($e->getMessage());
+        }
+    }
+
+    /**
+     * MÉTODO QUE NOS SIRVE PARA INSERTAR REGISTROS A LA TABLA EQUIPOS EN LA BASE DE DATOS
+     */
+    public function registrar($datos = []){
+        try{
+            $consulta = $this->conexion->prepare("CALL spu_equipos_registrar(?,?,?,?,?,?)");
+            $consulta->execute(
+                array(
+                    $datos['idcategoria'],
+                    $datos['idmarca'],
+                    $datos['idusuario'],
+                    $datos['modelo_equipo'],
+                    $datos['numero_serie'],
+                    $datos['imagen']
+                )
+            );
+
+            return $consulta->fetch(PDO::FETCH_ASSOC);
+        }
+        catch(Exception $e){
+            die($e->getMessage());
+        }
+    }
+
+    /**
+     * MÉTODO QUE NOS PERMITIRÁ MODIFICAR UN REGISTRO EN LA TABLA EQUIPOS, DE LA BASE DE DATOS
+     */
+    public function modificar($datos = []){
+        try{
+        $consulta = $this->conexion->prepare("CALL spu_equipos_modificar(?,?,?,?,?,?,?)");
+        $consulta->execute(
+            array(
+                $datos['idequipo'],
+                $datos['idcategoria'],
+                $datos['idmarca'],
+                $datos['idusuario'],
+                $datos['modelo_equipo'],
+                $datos['numero_serie'],
+                $datos['imagen']
+            )
+        );
 
         }
         catch(Exception $e){
             die($e->getMessage());
         }
     }
+
+    /**
+     * MÉTODO QUE NOS PERMITIRÁ ELIMINAR UN REGISTRO EN LA TABLA EQUIPOS DE LA BASE DE DATOS (INECTIVE_AT => NOW())
+     */
+    public function eliminar($datos = []){
+        try{
+            $consulta = $this->conexion->prepare("CALL spu_equipos_eliminar(?)");
+            $consulta->execute(array($datos['idequipo']));
+        }
+        catch(Exception $e){
+            die($e->getMessage());
+        }
+    }
+    
 }
