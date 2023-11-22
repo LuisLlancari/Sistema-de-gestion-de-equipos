9d69cf8708db14a0c91e2bef099f981e4f1bf499
Luis Llancari <miguel.llancari12@gmail.com>
2023-11-22 03:05:33 -0500

    cambiando modal cronograma

--- views/cronograma/cronograma.php
+++ views/cronograma/cronograma.php
@@ -28,8 +28,8 @@ require_once "../sidebar/sidebar.php";
     <div class="modal fade" id="modal-cronograma" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
-          <div class="modal-header text-center">
-            <h1 class="modal-title fs-5" id="titulo-modalC"></h1>
+          <div class="modal-header bg-dark">
+            <h1 class="modal-title fs-5 text-center text-white" id="titulo-modalC"></h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
@@ -75,17 +75,17 @@ require_once "../sidebar/sidebar.php";
             </div>
             
             <div class="mb-3">
-              <label for="comentario" class="form-label">Comentario</label>
+              <label for="comentario" class="form-label">Comentario:</label>
               <textarea class="form-control" id="comentario" rows="3" required ></textarea>
               <div class="invalid-feedback"> Ingresar comentario. </div>
             </div>
 
           </form>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-success" id="agregar"><i class="fi-sr-eye"></i>Enviar</button>
-             <button type="button" class="btn btn-danger"  id="borrar">Borrar</button>
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <div class="modal-footer d-flex justify-content-between">
+            <button type="button" class="btn btn-outline-success flex-fill" id="agregar"><i class="fi-sr-eye"></i>Guardar</button>
+             <button type="button" class="btn btn-outline-danger flex-fill"  id="borrar">Borrar</button>
+            <button type="button" class="btn btn-outline-secondary flex-fill" data-bs-dismiss="modal">Cerrar</button>
           </div>
         </div>
       </div>
